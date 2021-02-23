9163652e2c144f4644da220dd4095c3aa71be654
Des-sk <marquezdecardona@gmail.com>
2021-02-23 00:58:04 -0400

    commit tree 073c97b32ce3167e69c5d46e08b9677adf0fdf8d

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
-class User < ActiveRecord::Base
-  validates_presence_of :name, :email, :password
-end
+class CreateUsers < ActiveRecord::Migration[4.2]
+  def up
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+end 
